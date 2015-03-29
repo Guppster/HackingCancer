b62d80172bb9a4d2f715b16d17d9c642d9649983
Gurpreet Singh <Guppst3r@gmail.com>
2015-03-29 01:26:01 -0400

    Added Multiple Levels of Difficulty AI

--- src/Monster.java
+++ src/Monster.java
@@ -4,9 +4,12 @@ public class Monster extends Sprite
 {
     //The monster's height should be a little bit smaller than the player's height.
 
-    public Monster(Image image, Rectangle rectangle, String name, int jumptimer, int jumpPower)
+    private int level; //Level 0 = Easy; Level 1 = Smart; Level 2 = Impossible
+
+    public Monster(Image image, Rectangle rectangle, String name, int jumptimer, int jumpPower, int level)
     {
         super(image, rectangle, name, jumptimer, jumpPower);
+        this.level = level;
     }//End of constructor
     public Monster(Rectangle rect)
     {
@@ -26,44 +29,72 @@ public class Monster extends Sprite
 
         //Use players direction and velocity to determine the next move of the AI monster
 
-        //If Monster is on the right side of Player
-        if(monsterX - playerX > 300 || playerX - monsterX > 300)
+        if(level == 0)
         {
-            return;
+            //If Monster is on the right side of Player
+            if(monsterX - playerX > 500 || playerX - monsterX > 500)
+            {
+                return;
+            }
         }
+        else
+        {
+            //If Monster is on the right side of Player
+            if(monsterX - playerX > 300 || playerX - monsterX > 300)
+            {
+                return;
+            }
+        }
+
 
         if(monsterX > 10)
         {
             this.setVelocityX(this.getVelocityX() * -1);
         }
 
+        //when the monster is ahead of the player on the course
         if(playerX < monsterX)
         {
             if(player.isFacingRight() && playerVelocity > 0)
             {
                this.setVelocityX(1.5);
 
-                //Wait till he gets close (20 pixel approx. range)
-                if(monsterX - playerX < 20)
+                if(level == 1 || level == 2)
                 {
-                    //Stop and turn around
-                    this.setVelocityX(-1);
-
-                    //Jump at him!
-                    if(this.isGrounded())
+                    //Wait till he gets close (20 pixel approx. range)
+                    if(monsterX - playerX < 20)
                     {
-                        this.setGrounded(false);
-                        this.setJumpTimer(10);
-                        this.setVelocityY(this.getJumpPower());
+                        //Stop and turn around
+                        this.setVelocityX(-1);
+
+                        //Jump at him!
+                        if(this.isGrounded())
+                        {
+                            this.setGrounded(false);
+                            this.setJumpTimer(10);
+                            this.setVelocityY(this.getJumpPower());
+                        }
                     }
                 }
+                else
+                {
+                    this.setVelocityX(-1);
+                }
             }
-            else
-            {
-                //Chance (Max Positive Velocity)
-                this.setVelocityX(-1.5);
+            else {
+                if (level == 0)
+                {
+                    //Chance (Max Negative Velocity)
+                    this.setVelocityX(-1);
+                }
+                else
+                {
+                    //Chance (Max Negative Velocity)
+                    this.setVelocityX(-1.5);
+                }
+
             }
-        }
+        }//End of Monster ahead of Player method
 
         //If Player is on the right side of Monster
         if(playerX > monsterX)
@@ -72,29 +103,41 @@ public class Monster extends Sprite
             {
                 this.setVelocityX(-1.5);
 
-                //Wait till he gets close (20 pixel approx. range)
-                if(playerX - monsterX < 20)
-                {
-                    //Stop and turn around
-                    this.setVelocityX(1);
+                if(level == 1 || level == 2) {
+                    //Wait till he gets close (20 pixel approx. range)
+                    if (playerX - monsterX < 20) {
+                        //Stop and turn around
+                        this.setVelocityX(1);
 
-                    //Jump at him!
-                    if(this.isGrounded())
-                    {
-                        this.setGrounded(false);
-                        this.setJumpTimer(10);
-                        this.setVelocityY(this.getJumpPower());
+                        //Jump at him!
+                        if (this.isGrounded()) {
+                            this.setGrounded(false);
+                            this.setJumpTimer(10);
+                            this.setVelocityY(this.getJumpPower());
+                        }
                     }
+                }
+                else
+                {
+                    this.setVelocityX(1);
                 }
             }
             else
             {
-                //Chase (Max Positive Velocity)
-                this.setVelocityX(1.5);
+                if (level == 0)
+                {
+                    //Chance (Max Positive Velocity)
+                    this.setVelocityX(1);
+                }
+                else
+                {
+                    //Chance (Max Positive Velocity)
+                    this.setVelocityX(1.5);
+                }
             }
         }
         //If the player is jumping within range of monster, monster jumps to hit from underneath before player can jump over
-        else if(playerY > monsterY && (((playerX > monsterX) && playerX - monsterX < 20) || (playerX < monsterX) && monsterX - playerX < 20))
+        else if((playerY > monsterY && (((playerX > monsterX) && playerX - monsterX < 20) || (playerX < monsterX) && monsterX - playerX < 20)) && level == 2)
         {
             //Jump from underneath and kill player
             if(this.isGrounded())
@@ -105,4 +148,12 @@ public class Monster extends Sprite
             }
         }
     }//End of nextMove method
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
 }//End of monster class
